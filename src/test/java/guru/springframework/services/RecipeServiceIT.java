1fb3dee63e99a61a440122229cd022515e0a5d4e
anookinov <anookinov@gmail.com>
2021-06-14 00:57:49 +0700

    convert to mongodb and adjust source code and test code

--- src/test/java/guru/springframework/services/RecipeServiceIT.java
+++ src/test/java/guru/springframework/services/RecipeServiceIT.java
@@ -9,6 +9,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +20,7 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by jt on 6/21/17.
  */
+//@DataMongoTest
 @Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -38,7 +40,6 @@ public class RecipeServiceIT {
     @Autowired
     RecipeToRecipeCommand recipeToRecipeCommand;
 
-    @Transactional
     @Test
     public void testSaveOfDescription() throws Exception {
         //given
